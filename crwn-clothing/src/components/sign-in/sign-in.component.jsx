8b0b41dfef1ffd4677586015614a41853310c49b
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-29 22:31:38 +0100

    create a custom button component

--- crwn-clothing/src/components/sign-in/sign-in.component.jsx
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import FormInput from '../form-input/form-input.component';
+import CustomButton from '../custom-button/custom-button.component';
 
 import './sign-in.styles.scss';
 
@@ -45,7 +46,7 @@ class SignIn extends Component {
             required
           />
 
-          <input type="submit" value="Submit" />
+          <CustomButton type="submit">Sign In</CustomButton>
         </form>
       </div>
     );
