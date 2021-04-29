155827b88529315da1e89d83a94bdc625ee392dc
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-29 21:50:08 +0100

    create sign in component

---
+++ crwn-clothing/src/pages/sign-in-up/sign-in-up.component.jsx
@@ -0,0 +1,14 @@
+import React from 'react';
+import SignIn from '../../components/sign-in/sign-in.component';
+
+import './sign-in-up.styles.scss';
+
+const SignInPage = () => {
+  return (
+    <div className="sign-in">
+      <SignIn />
+    </div>
+  );
+};
+
+export default SignInPage;
