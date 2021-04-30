a6f904ca19cd6a4aa2ea88a7d56b2801e66c90ef
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-04-30 23:27:30 +0100

    create cart dropdown component

---
+++ crwn-clothing/src/components/cart-dropdown/cart-dropdown.component.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+
+import CustomButtom from '../custom-button/custom-button.component';
+
+import './cart-dropdown.styles.scss';
+
+const CartDropdown = () => {
+  return (
+    <div className="cart-dropdown">
+      <div className="cart-items"></div>
+      <CustomButtom>GO TO CHECKOUT</CustomButtom>
+    </div>
+  );
+};
+
+export default CartDropdown;
