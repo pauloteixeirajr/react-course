ebdb0499f38b70dd4e4b31c56ba75f4a9fc0573a
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-05-01 23:20:43 +0100

    removing items from the cart

--- crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
+++ crwn-clothing/src/components/checkout-item/checkout-item.component.jsx
@@ -1,8 +1,11 @@
 import React from 'react';
+import { connect } from 'react-redux';
+import { clearItemFromCart } from '../../redux/cart/cart.actions';
 
 import './checkout-item.styles.scss';
 
-const CheckoutItem = ({ item: { name, imageUrl, price, quantity } }) => {
+const CheckoutItem = ({ item, clearItem }) => {
+  const { name, imageUrl, price, quantity } = item;
   return (
     <div className="checkout-item">
       <div className="image-container">
@@ -11,9 +14,15 @@ const CheckoutItem = ({ item: { name, imageUrl, price, quantity } }) => {
       <span className="name">{name}</span>
       <span className="quantity">{quantity}</span>
       <span className="price">{price}</span>
-      <div className="remove-button">&#10005;</div>
+      <div onClick={() => clearItem(item)} className="remove-button">
+        &#10005;
+      </div>
     </div>
   );
 };
 
-export default CheckoutItem;
+const mapDispatchToProps = dispatch => ({
+  clearItem: item => dispatch(clearItemFromCart(item)),
+});
+
+export default connect(null, mapDispatchToProps)(CheckoutItem);
