f70a26b06db5c6087a5f51fa4b88084d0da1193c
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-05-02 14:40:20 +0100

    creating the collection overview component

--- crwn-clothing/src/pages/shop/shop.component.jsx
+++ crwn-clothing/src/pages/shop/shop.component.jsx
@@ -1,21 +1,12 @@
 import React from 'react';
-import { connect } from 'react-redux';
-import { createStructuredSelector } from 'reselect';
-import { selectCollections } from '../../redux/shop/shop.selectors';
-import CollectionPreview from '../../components/collection-preview/collection-preview.component';
+import CollectionsOverview from '../../components/collections-overview/collections-overview.component';
 
-const ShopPage = ({ collections }) => {
+const ShopPage = () => {
   return (
     <div className="shop-page">
-      {collections.map(({ id, ...otherProps }) => (
-        <CollectionPreview key={id} {...otherProps} />
-      ))}
+      <CollectionsOverview />
     </div>
   );
 };
 
-const mapStateToPros = createStructuredSelector({
-  collections: selectCollections,
-});
-
-export default connect(mapStateToPros)(ShopPage);
+export default ShopPage;
