6bce20bdc0cd494d507c0c59cf116b3c5b92d8d5
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-05-03 21:48:52 +0100

    clear cart with signout in a saga

--- crwn-clothing/src/redux/cart/cart.reducer.js
+++ crwn-clothing/src/redux/cart/cart.reducer.js
@@ -30,6 +30,11 @@ const cartReducer = (state = initialState, action) => {
           item => item.id !== action.payload.id
         ),
       };
+    case CartActionTypes.CLEAR_CART:
+      return {
+        ...state,
+        cartItems: [],
+      };
     default:
       return state;
   }
