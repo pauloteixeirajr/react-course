b585e128a7a4a235db9859a4425cc8d93845fadc
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-05-04 22:21:13 +0100

    backend payment route

--- crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
+++ crwn-clothing/src/components/stripe-button/stripe-button.component.jsx
@@ -18,7 +18,7 @@ const StripeButton = ({ price }) => {
       name="Crwn Clothing Ltd."
       billingAddress
       shippingAddress
-      image="https://sendeyo.com/up/d/f3eb2117da"
+      image="https://svgshare.com/i/CUz.svg"
       description={`Your total is $${price}`}
       amount={priceForStripe}
       panelLabel="Pay Now"
