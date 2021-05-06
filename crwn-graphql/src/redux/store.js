a7a3cee03c1a56f4bcdd41336fae0c0a8f7f31a2
Paulo Teixeira <pauloteixeirajr@outlook.com>
2021-05-06 22:17:54 +0100

    setting up apollo and querying the graphql server

--- crwn-graphql/src/redux/store.js
+++ crwn-graphql/src/redux/store.js
@@ -14,4 +14,6 @@ export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 
 export const persistor = persistStore(store);
 
-export default { store, persistStore };
+const storeState = { store, persistStore };
+
+export default storeState;
